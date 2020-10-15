19b988be5fc9681b4212bf34bedc97d1bf28b73a
Osehi <osehiaseehilen@gmail.com>
2020-10-15 08:55:59 +0100

    tour screen two central image

---
+++ app/src/main/java/www/mc/com/TourScreenTwoFragment.kt
@@ -0,0 +1,28 @@
+package www.mc.com
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class TourScreenTwoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tour_screen_two, container, false)
+    }
+
+
+}
