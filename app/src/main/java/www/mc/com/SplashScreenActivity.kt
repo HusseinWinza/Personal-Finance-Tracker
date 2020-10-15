569ba94cce1cc438f3a0c8468a677e62e10ecba0
didahdx <didahdaniel@gmail.com>
2020-10-15 11:29:43 +0300

    added splash screen to launch activity

--- app/src/main/java/www/mc/com/SplashScreenActivity.kt
+++ app/src/main/java/www/mc/com/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package www.mc.com
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
